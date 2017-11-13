42cbb7d88c417f2d7fe8f9199e5185fc8bf98947
Daniel Schoonover <daniel.c.schoonover@gmail.com>
2017-11-13 17:12:20 -0500

    Center + Standardize
    centers and standardizes feature matrices for training

--- fStandardizeML.m
+++ fStandardizeML.m
@@ -1,23 +1,23 @@
-function[Xts,Xtr]=fStandardizeML(Xts,Xtr)
+function[Xts,Xtr]=fStandardizeML(Xtr,Xts)
 %fStandardizeML centers and standardizes training (Xtr) and test(Xts) data. 
-% Xtr, Xts are matrixes ~ [D x N], 
-% where D is # features/dimensions, N is # instances/samples.
-% Standardization centers to zero and standard deviation to 1. 
-% It is often applied as a preprocessing step on data before feeding into learning 
+% Xtr, Xts are matrixes ~ [N X D], D  = #dimensions, N = #instances/samples.
+% This is often applied as a preprocessing step on data before feeding into learning 
 % algorithms.  % fStandardizeML requires separate training and test matrices
 % because noobs often forget to separate test data from preprocessing steps.
 
-
-[D,N] = size(X);
+[N,D] = size(Xtr);
 
 %CENTER both matrices w/ params from Xtr:
-Xtr=Xtr-repmat(mean(Xtr'),size(Xtr,2),1)';
-Xts=Xts-repmat(mean(Xtr'),size(Xts,2),1)'; 
+Xtr=Xtr-repmat(mean(Xtr),N,1);
+Xts=Xts-repmat(mean(Xtr),size(Xts,1),1);
+
 %now both matrices have zero mean along dimensions D.
 
 %STANDARDIZE both matrices w/ params from Xtr.
-Xtr=Xtr./repmat(std(Xtr'),size(Xtr,2),1)';
-Xts=Xts./repmat(std(Xtr'),size(Xts,2),1)';
-%now both matrices have unit variance for each dimension D
+Xtr=Xtr./repmat(std(Xtr),N,1);
+
+Xts=Xts./repmat(std(Xtr),size(Xts,1),1);
+
+%now both matrices have unit variance along each dimension D
 
 return 
